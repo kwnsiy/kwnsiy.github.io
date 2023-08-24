42fef709f14dd97061d9a18655c02d9b501d8b4e
kwnsiy <sei5512kkk@yahoo.co.jp>
2023-08-24 12:48:04 +0000

    new

--- asr.py
+++ asr.py
@@ -48,7 +48,7 @@ merged = pd.merge(dataset, vfd, left_on=['image', 'original_transcript'], right_
 merged_grouped = merged.groupby(['image', 'original_transcript'])['verbal_response'].apply('||'.join).reset_index()
 
 # 結合したデータフレームを 'dataset' とマージして、新しい 'response' カラムを作成
-final_merged = pd.merge(dataset, merged_grouped, on=['image', 'original_transcript'])
+final_merged = pd.merge(dataset, merged_grouped, on=['image', 'original_transcript'], how='inner')
 final_merged.rename(columns={'verbal_response': 'response'}, inplace=True)
 
 
